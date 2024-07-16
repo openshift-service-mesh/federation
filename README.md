f8a983223632097395d820b3c862859fc425038a
Jacek Ewertowski <jewertow@redhat.com>
2024-07-16 11:58:47 +0200

    Take JSON input instead of YAML
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

--- README.md
+++ README.md
@@ -1,23 +1,16 @@
 ### About the project
 
-TODO
+`Federation` is a controller that utilizes MCP protocol to configure mesh-federation in Istio.
 
-### Example of run 
-./out/federation-controller --meshPeers "spec: 
-              remote: 
-                addresses: 
-                - lb-1234567890.us-east-1.elb.amazonaws.com
-                - 192.168.10.56
-                ports: 
-                  dataPlane: 15443
-                  discovery: 15020" --exportedServiceSet "rules: 
-          - type: LabelSelector
-            labelSelectors:
-              - matchLabels:
-                  export-service: \"exportServiceGv\"
-              - matchExpressions: {}" --importedServiceSet "rules: 
-          - type: LabelSelector
-            labelSelectors:
-              - matchLabels:
-                  export-service: \"importServiceGv\"
-              - matchExpressions: {}"
+### Development
+1. Build:
+```shell
+make
+```
+2. Run locally:
+```shell
+./out/federation-controller \
+  --meshPeers '{"spec":{"remote":{"addresses": ["lb-1234567890.us-east-1.elb.amazonaws.com","192.168.10.56"],"ports":{"dataPlane":15443,"discoery":15020}}}}'\
+  --exportedServiceSet '{"type":"LabelSelector","labelSelectors":[{"matchLabels":{"export-service":"true"}}]}'\
+  --importedServiceSet '{"type":"LabelSelector","labelSelectors":[{"matchLabels":{"export-service":"true"}}]}'
+```
--- cmd/federation-controller/main.go
+++ cmd/federation-controller/main.go
@@ -1,60 +1,70 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"os"
 	"github.com/jewertow/federation/internal/pkg/config"
 	server "github.com/jewertow/federation/internal/pkg/xds"
-	"gopkg.in/yaml.v3"
+	"log"
+	"os"
 	"os/signal"
 	"syscall"
-	"context"
-	"log"
 )
 
 // Global variable to store the parsed arguments and "flag" arguments
 var (
-	meshPeer = flag.String("meshPeers", "meshPeers Yml",
-		"MeshPeers that include address ip/hostname to remote Peer, and the ports for dataplane and discovery")
-	exportedServiceSet = flag.String("exportedServiceSet", "exportedServiceSet Yml",
+	meshPeer = flag.String("meshPeers", "",
+		"Mesh peers that include address ip/hostname to remote Peer, and the ports for dataplane and discovery")
+	exportedServiceSet = flag.String("exportedServiceSet", "",
 		"ExportedServiceSet that include selectors to match the services that will be exported")
-	importedServiceSet = flag.String("importedServiceSet", "importedServiceSet Yml",
+	importedServiceSet = flag.String("importedServiceSet", "",
 		"ImportedServiceSet that include selectors to match the services that will be imported")
 )
 
-// unmarshalYAML is a utility function to unmarshal a YAML string into a struct
+// unmarshalJSON is a utility function to unmarshal a YAML string into a struct
 // and return an error if the unmarshalling fails.
-func unmarshalYAML(yamlStr string, out interface{}) {
-	if err := yaml.Unmarshal([]byte(yamlStr), out); err != nil {
-		fmt.Printf("Error unmarshalling : %v\n", err)
-		os.Exit(-1)
+func unmarshalJSON(input string, out interface{}) error {
+	if err := json.Unmarshal([]byte(input), out); err != nil {
+		return fmt.Errorf("failed to unmarshal JSON: %w", err)
 	}
+	return nil
 }
 
 // Parse the command line arguments by using the flag package
 // Export the parsed arguments to the AppConfig variable
-func parse() *config.Federation {
+func parse() (*config.Federation, error) {
 	var (
 		peers    config.MeshPeers
 		exported config.ExportedServices
 		imported config.ImportedServices
 	)
 
-	unmarshalYAML(*meshPeer, &peers)
-	unmarshalYAML(*exportedServiceSet, &exported)
-	unmarshalYAML(*importedServiceSet, &imported)
+	if err := unmarshalJSON(*meshPeer, &peers); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal mesh peers: %w", err)
+	}
+	if err := unmarshalJSON(*exportedServiceSet, &exported); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal exported services: %w", err)
+	}
+	if err := unmarshalJSON(*importedServiceSet, &imported); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal imported services: %w", err)
+	}
 
 	return &config.Federation{
 		MeshPeers:          peers,
 		ExportedServiceSet: exported,
 		ImportedServiceSet: imported,
-	}
+	}, nil
 }
 
 func main() {
 	flag.Parse()
-	parse()
+	cfg, err := parse()
+	if err != nil {
+		os.Exit(1)
+	}
+	fmt.Println("Configuration: ", cfg)
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancel()
@@ -63,5 +73,5 @@ func main() {
 		log.Fatal("Error starting server: ", err)
 	}
 
-    os.Exit(0)
+	os.Exit(0)
 }
