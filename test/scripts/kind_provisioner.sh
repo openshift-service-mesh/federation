4b7e14d201ab1010cdfc2becb20c271962908b55
Jacek Ewertowski <jewertow@redhat.com>
2024-10-31 15:20:34 +0100

    Improve kind_provisioner to discover existing clusters and skip provisioning if not needed
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

--- test/scripts/kind_provisioner.sh
+++ test/scripts/kind_provisioner.sh
@@ -15,6 +15,23 @@ istio_version=$1
 
 source "$ROOT/scripts/lib.sh"
 
+found_clusters=0
+for name in "east" "west"; do
+  if kind get clusters | grep -q "$name"; then
+    found_clusters=$((found_clusters+1))
+  else
+    echo "Not found cluster $name"
+  fi
+done
+
+if [ "$found_clusters" -eq "2" ]; then
+  echo "All clusters were found - skipping clusters provisioning."
+  exit 0
+elif [ "$found_clusters" -ne "0" ]; then
+  echo "Did not find all clusters, but some exist - cleanup environment and run script again."
+  exit 1
+fi
+
 kind_pids=()
 create_kind_cluster east 10.10.0.0/16 10.255.10.0/24 &
 kind_pids[0]=$!
