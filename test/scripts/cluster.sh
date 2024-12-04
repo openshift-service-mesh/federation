040650062039b8f748473d7a453bd93d8c654076
Bartosz Majsak <bartosz.majsak@gmail.com>
2024-12-04 13:12:23 +0100

    feat(script):  subnet calucation for multiple clusters  (#99)
    * chore(scripts): renames subnet script
    
    * feat(script): reworks subnet calucations to support multiple clusters
    
    Previous implementation was tailored to support only two subnets for
    east and west clusters.
    
    This change simplifies subnet calculation and makes it capable of
    calculating subnets for more than 2 clusters.
    
    * fix: removes additional subnets

--- test/scripts/cluster.sh
+++ test/scripts/cluster.sh
@@ -2,17 +2,48 @@
 
 ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 
+provision_kind_clusters() {
+  local clusters=("$@")
+
+  pids=()
+  for cluster in "${clusters[@]}"; do
+    provision_kind_cluster "${cluster}" &
+    pids+=($!)
+  done
+
+  for pid in "${pids[@]}"; do
+    wait "$pid" || { echo "Failed provisioning kind cluster (pid: $pid)"; exit 1; };
+  done
+
+  pids=()
+  for cluster in "${clusters[@]}"; do
+    retry install_metallb "${cluster}" "${clusters[*]}" &
+    pids+=($!)
+  done
+
+  for pid in "${pids[@]}"; do
+    wait "$pid" || { echo "Failed provisioning kind cluster (pid: $pid)"; exit 1; };
+  done
+}
+
 provision_kind_cluster() {
   local cluster=$1
 
   kind create cluster --name "$cluster"
   kind get kubeconfig --name "${cluster}" > $ROOT/${cluster}.kubeconfig
-
-  retry install_metallb $cluster
 }
 
+# Installs and configures MetalLB on a specified Kubernetes cluster.
+#
+# Args:
+#   cluster (string): The name of the Kubernetes cluster where MetalLB should be installed.
+#   clusters (string): A comma-separated list of all region clusters, used to calculate a unique CIDR range for the cluster.
+#
+# Example:
+#   install_metallb "us-east-1" "us-east-1,us-west-1,eu-central-1"
 install_metallb() {
   local cluster=$1
+  local clusters=$2
 
   echo "Installing MetalLB for $cluster"
 
@@ -20,7 +51,7 @@ install_metallb() {
   kubectl --kubeconfig="$ROOT/$cluster.kubeconfig" wait -n metallb-system pod --timeout=120s -l app=metallb --for=condition=Ready
 
   local docker_kind_ipv4_subnet="$(docker inspect kind | jq '.[0].IPAM.Config' -r | jq -r '.[] | select(.Subnet | test("^[0-9]+\\.")) | .Subnet')"
-  local cidr=$(python3 "$ROOT/scripts/find_smaller_subnets.py" --network "$docker_kind_ipv4_subnet" --region "$cluster")
+  local cidr=$(python3 "$ROOT/scripts/find_subnets.py" --network "$docker_kind_ipv4_subnet" --region $cluster --regions $clusters)
 
   echo '
 apiVersion: metallb.io/v1beta1
---
+++ test/scripts/find_subnets.py
@@ -0,0 +1,43 @@
+import ipaddress
+import argparse
+import math
+import sys
+
+def find_subnets(network, required_subnets):
+    try:
+        network = ipaddress.ip_network(network, strict=False)
+        bits_needed = math.ceil(math.log2(required_subnets))
+
+        new_mask = network.prefixlen + bits_needed
+        if new_mask > network.max_prefixlen:
+            raise ValueError("Cannot create that many subnets with the given network.")
+
+        return list(network.subnets(new_prefix=new_mask))
+    except ValueError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+def main():
+    parser = argparse.ArgumentParser(description='Find CIDR subnets for a given network address.')
+    parser.add_argument('--network', type=str, required=True, help='The network address in CIDR notation (e.g., 172.18.0.0/16)')
+    parser.add_argument('--region', type=str, required=True, help='The k8s cluster region (e.g., west, east, etc.)')
+    parser.add_argument('--regions', nargs='+', required=True, help='Comma-separated list of regions (e.g., west,east,central)')
+
+    args = parser.parse_args()
+
+    if args.region not in args.regions:
+        print(f"Unknown region: {args.region}")
+        sys.exit(1)
+
+    required_subnets = len(args.regions)
+    subnets = find_subnets(args.network, required_subnets)
+
+    if not subnets or len(subnets) < required_subnets:
+        print(f"Failed to generate subnets: {subnets}")
+        sys.exit(1)
+
+    region_index = args.regions.index(args.region)
+    print(subnets[region_index])
+
+if __name__ == "__main__":
+    main()
