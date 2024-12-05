12fc021519b054400cde4ac393f778bd3acc9ab8
Bartosz Majsak <bartosz.majsak@gmail.com>
2024-12-05 22:30:00 +0100

    feat(config): controller's namespace autodiscovery (#102)
    We do not need special environment variable to determine where our
    controller runs. To achieve this we could either use Downward API
    environment variable called `POD_NAMESPACE` or rely on the fact that we
    are using Service Account which automatically propagates namespace as
    mounted secret.
    
    This PR introduces `config.Namespace` function which resolve namespace
    in the exact order mentioned above or falls back to `istio-system`.
    
    Bonus: it also re-uses Istio's underyling k8s client for the informers
    instead of creating new instance.

---
+++ internal/pkg/config/cluster.go
@@ -0,0 +1,36 @@
+// Copyright Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the License);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an AS IS BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"os"
+	"strings"
+)
+
+func Namespace() string {
+	// First, check POD_NAMESPACE environment variable if Downward API is used
+	if ns, defined := os.LookupEnv("POD_NAMESPACE"); defined {
+		return ns
+	}
+
+	// Next, if using service account token, rely on mounted secret
+	if data, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace"); err == nil {
+		if ns := strings.TrimSpace(string(data)); ns != "" {
+			return ns
+		}
+	}
+
+	return "istio-system"
+}
