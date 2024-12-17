666c35185b68d5729954f2544bd595a50455926e
bartoszmajsak <bartosz.majsak@gmail.com>
2024-12-17 09:33:53 +0100

    wip(multi-peer): config factory handles multiple remotes

--- internal/pkg/istio/config_factory.go
+++ internal/pkg/istio/config_factory.go
@@ -63,54 +63,60 @@ func NewConfigFactory(
 // DestinationRules customize SNI in the client mTLS connection when the remote ingress is openshift-router,
 // because that ingress requires hosts compatible with https://datatracker.ietf.org/doc/html/rfc952.
 func (cf *ConfigFactory) DestinationRules() []*v1alpha3.DestinationRule {
-	remote := cf.cfg.MeshPeers.Remotes[0]
 
-	if remote.IngressType != config.OpenShiftRouter {
-		return nil
-	}
+	var destinationRules []*v1alpha3.DestinationRule
 
-	createObjectMeta := func(svcName, svcNs string) metav1.ObjectMeta {
-		return metav1.ObjectMeta{
-			Name:      fmt.Sprintf("mtls-sni-%s-%s", svcName, svcNs),
-			Namespace: cf.cfg.MeshPeers.Local.ControlPlane.Namespace,
-			Labels:    map[string]string{"federation.openshift-service-mesh.io/peer": "todo"},
+	for _, remote := range cf.cfg.MeshPeers.Remotes {
+		if remote.IngressType != config.OpenShiftRouter {
+			continue
 		}
-	}
 
-	destinationRules := []*v1alpha3.DestinationRule{{
-		ObjectMeta: createObjectMeta(remote.ServiceName(), "istio-system"),
-		Spec: istionetv1alpha3.DestinationRule{
-			Host: remote.ServiceFQDN(),
-			TrafficPolicy: &istionetv1alpha3.TrafficPolicy{
-				Tls: &istionetv1alpha3.ClientTLSSettings{
-					Mode: istionetv1alpha3.ClientTLSSettings_ISTIO_MUTUAL,
-					Sni:  routerCompatibleSNI(remote.ServiceName(), "istio-system", remote.ServicePort()),
-				},
-			},
-		},
-	}}
+		createObjectMeta := func(svcName, svcNs string) metav1.ObjectMeta {
+			return metav1.ObjectMeta{
+				Name:      fmt.Sprintf("mtls-sni-%s-%s", svcName, svcNs),
+				Namespace: cf.cfg.MeshPeers.Local.ControlPlane.Namespace,
+				Labels:    map[string]string{"federation.openshift-service-mesh.io/peer": "todo"},
+			}
+		}
 
-	for _, svc := range cf.importedServiceStore.From(remote) {
-		dr := &v1alpha3.DestinationRule{
-			ObjectMeta: createObjectMeta(svc.Name, svc.Namespace),
+		drPerRemote := []*v1alpha3.DestinationRule{{
+			ObjectMeta: createObjectMeta(remote.ServiceName(), "istio-system"),
 			Spec: istionetv1alpha3.DestinationRule{
-				Host: fmt.Sprintf("%s.%s.svc.cluster.local", svc.Name, svc.Namespace),
+				Host: remote.ServiceFQDN(),
 				TrafficPolicy: &istionetv1alpha3.TrafficPolicy{
-					PortLevelSettings: []*istionetv1alpha3.TrafficPolicy_PortTrafficPolicy{},
+					Tls: &istionetv1alpha3.ClientTLSSettings{
+						Mode: istionetv1alpha3.ClientTLSSettings_ISTIO_MUTUAL,
+						Sni:  routerCompatibleSNI(remote.ServiceName(), "istio-system", remote.ServicePort()),
+					},
 				},
 			},
-		}
-		for _, port := range svc.Ports {
-			dr.Spec.TrafficPolicy.PortLevelSettings = append(dr.Spec.TrafficPolicy.PortLevelSettings, &istionetv1alpha3.TrafficPolicy_PortTrafficPolicy{
-				Port: &istionetv1alpha3.PortSelector{Number: port.Number},
-				Tls: &istionetv1alpha3.ClientTLSSettings{
-					Mode: istionetv1alpha3.ClientTLSSettings_ISTIO_MUTUAL,
-					Sni:  routerCompatibleSNI(svc.Name, svc.Namespace, port.Number),
+		}}
+
+		for _, svc := range cf.importedServiceStore.From(remote) {
+			dr := &v1alpha3.DestinationRule{
+				ObjectMeta: createObjectMeta(svc.Name, svc.Namespace),
+				Spec: istionetv1alpha3.DestinationRule{
+					Host: fmt.Sprintf("%s.%s.svc.cluster.local", svc.Name, svc.Namespace),
+					TrafficPolicy: &istionetv1alpha3.TrafficPolicy{
+						PortLevelSettings: []*istionetv1alpha3.TrafficPolicy_PortTrafficPolicy{},
+					},
 				},
-			})
+			}
+			for _, port := range svc.Ports {
+				dr.Spec.TrafficPolicy.PortLevelSettings = append(dr.Spec.TrafficPolicy.PortLevelSettings, &istionetv1alpha3.TrafficPolicy_PortTrafficPolicy{
+					Port: &istionetv1alpha3.PortSelector{Number: port.Number},
+					Tls: &istionetv1alpha3.ClientTLSSettings{
+						Mode: istionetv1alpha3.ClientTLSSettings_ISTIO_MUTUAL,
+						Sni:  routerCompatibleSNI(svc.Name, svc.Namespace, port.Number),
+					},
+				})
+			}
+			drPerRemote = append(drPerRemote, dr)
 		}
-		destinationRules = append(destinationRules, dr)
+
+		destinationRules = append(destinationRules, drPerRemote...)
 	}
+
 	return destinationRules
 }
 
@@ -223,97 +229,102 @@ func (cf *ConfigFactory) EnvoyFilters() []*v1alpha3.EnvoyFilter {
 
 func (cf *ConfigFactory) ServiceEntries() ([]*v1alpha3.ServiceEntry, error) {
 
-	remote := cf.cfg.MeshPeers.Remotes[0]
+	var serviceEntries []*v1alpha3.ServiceEntry
 
-	if len(remote.Addresses) == 0 {
-		return nil, nil
-	}
+	for _, remote := range cf.cfg.MeshPeers.Remotes {
+		if len(remote.Addresses) == 0 {
+			continue
+		}
 
-	var resolution istionetv1alpha3.ServiceEntry_Resolution
-	if networking.IsIP(remote.Addresses[0]) {
-		resolution = istionetv1alpha3.ServiceEntry_STATIC
-	} else {
-		resolution = istionetv1alpha3.ServiceEntry_DNS
-	}
+		var resolution istionetv1alpha3.ServiceEntry_Resolution
+		if networking.IsIP(remote.Addresses[0]) {
+			resolution = istionetv1alpha3.ServiceEntry_STATIC
+		} else {
+			resolution = istionetv1alpha3.ServiceEntry_DNS
+		}
 
-	serviceEntries := []*v1alpha3.ServiceEntry{cf.serviceEntryForRemoteFederationController()}
-	for _, importedSvc := range cf.importedServiceStore.From(remote) {
-		_, err := cf.serviceLister.Services(importedSvc.Namespace).Get(importedSvc.Name)
-		if err != nil {
-			if !errors.IsNotFound(err) {
-				return nil, fmt.Errorf("failed to get Service %s/%s: %w", importedSvc.Name, importedSvc.Namespace, err)
-			}
-			// Service doesn't exist - create ServiceEntry.
-			var ports []*istionetv1alpha3.ServicePort
-			for _, port := range importedSvc.Ports {
-				ports = append(ports, &istionetv1alpha3.ServicePort{
-					Name:       port.Name,
-					Number:     port.Number,
-					Protocol:   port.Protocol,
-					TargetPort: port.TargetPort,
+		serviceEntriesPerRemote := []*v1alpha3.ServiceEntry{cf.serviceEntryForRemoteFederationController(remote)}
+		for _, importedSvc := range cf.importedServiceStore.From(remote) {
+			_, err := cf.serviceLister.Services(importedSvc.Namespace).Get(importedSvc.Name)
+			if err != nil {
+				if !errors.IsNotFound(err) {
+					return nil, fmt.Errorf("failed to get Service %s/%s: %w", importedSvc.Name, importedSvc.Namespace, err)
+				}
+				// Service doesn't exist - create ServiceEntry.
+				var ports []*istionetv1alpha3.ServicePort
+				for _, port := range importedSvc.Ports {
+					ports = append(ports, &istionetv1alpha3.ServicePort{
+						Name:       port.Name,
+						Number:     port.Number,
+						Protocol:   port.Protocol,
+						TargetPort: port.TargetPort,
+					})
+				}
+				serviceEntriesPerRemote = append(serviceEntriesPerRemote, &v1alpha3.ServiceEntry{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      fmt.Sprintf("import-%s-%s", importedSvc.Name, importedSvc.Namespace),
+						Namespace: cf.cfg.MeshPeers.Local.ControlPlane.Namespace,
+						Labels:    map[string]string{"federation.openshift-service-mesh.io/peer": "todo"},
+					},
+					Spec: istionetv1alpha3.ServiceEntry{
+						Hosts: []string{fmt.Sprintf("%s.%s.svc.cluster.local", importedSvc.Name, importedSvc.Namespace)},
+						Ports: ports,
+						Endpoints: slices.Map(remote.Addresses, func(addr string) *istionetv1alpha3.WorkloadEntry {
+							return &istionetv1alpha3.WorkloadEntry{
+								Address: addr,
+								Labels:  maps.MergeCopy(importedSvc.Labels, map[string]string{"security.istio.io/tlsMode": "istio"}),
+								Ports:   makePortsMap(importedSvc.Ports, remote.GetPort()),
+								Network: remote.Network,
+							}
+						}),
+						Location:   istionetv1alpha3.ServiceEntry_MESH_INTERNAL,
+						Resolution: resolution,
+					},
 				})
 			}
-			serviceEntries = append(serviceEntries, &v1alpha3.ServiceEntry{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      fmt.Sprintf("import-%s-%s", importedSvc.Name, importedSvc.Namespace),
-					Namespace: cf.cfg.MeshPeers.Local.ControlPlane.Namespace,
-					Labels:    map[string]string{"federation.openshift-service-mesh.io/peer": "todo"},
-				},
-				Spec: istionetv1alpha3.ServiceEntry{
-					Hosts: []string{fmt.Sprintf("%s.%s.svc.cluster.local", importedSvc.Name, importedSvc.Namespace)},
-					Ports: ports,
-					Endpoints: slices.Map(remote.Addresses, func(addr string) *istionetv1alpha3.WorkloadEntry {
-						return &istionetv1alpha3.WorkloadEntry{
-							Address: addr,
-							Labels:  maps.MergeCopy(importedSvc.Labels, map[string]string{"security.istio.io/tlsMode": "istio"}),
-							Ports:   makePortsMap(importedSvc.Ports, remote.GetPort()),
-							Network: remote.Network,
-						}
-					}),
-					Location:   istionetv1alpha3.ServiceEntry_MESH_INTERNAL,
-					Resolution: resolution,
-				},
-			})
 		}
+
+		serviceEntries = append(serviceEntries, serviceEntriesPerRemote...)
 	}
+
 	return serviceEntries, nil
 }
 
 func (cf *ConfigFactory) WorkloadEntries() ([]*v1alpha3.WorkloadEntry, error) {
-	remote := cf.cfg.MeshPeers.Remotes[0]
-
 	var workloadEntries []*v1alpha3.WorkloadEntry
 
-	for _, importedSvc := range cf.importedServiceStore.From(remote) {
-		_, err := cf.serviceLister.Services(importedSvc.Namespace).Get(importedSvc.Name)
-		if err != nil {
-			if !errors.IsNotFound(err) {
-				return nil, fmt.Errorf("failed to get Service %s/%s: %w", importedSvc.Name, importedSvc.Namespace, err)
-			}
-		} else {
-			// Service already exists - create WorkloadEntries.
-			for idx, ip := range networking.Resolve(remote.Addresses...) {
-				workloadEntries = append(workloadEntries, &v1alpha3.WorkloadEntry{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      fmt.Sprintf("import-%s-%d", importedSvc.Name, idx),
-						Namespace: importedSvc.Namespace,
-						Labels:    map[string]string{"federation.openshift-service-mesh.io/peer": "todo"},
-					},
-					Spec: istionetv1alpha3.WorkloadEntry{
-						Address: ip,
-						Labels:  maps.MergeCopy(importedSvc.Labels, map[string]string{"security.istio.io/tlsMode": "istio"}),
-						Ports:   makePortsMap(importedSvc.Ports, remote.GetPort()),
-						Network: remote.Network,
-					},
-				})
+	for _, remote := range cf.cfg.MeshPeers.Remotes {
+		for _, importedSvc := range cf.importedServiceStore.From(remote) {
+			_, err := cf.serviceLister.Services(importedSvc.Namespace).Get(importedSvc.Name)
+			if err != nil {
+				if !errors.IsNotFound(err) {
+					return nil, fmt.Errorf("failed to get Service %s/%s: %w", importedSvc.Name, importedSvc.Namespace, err)
+				}
+			} else {
+				// Service already exists - create WorkloadEntries.
+				for idx, ip := range networking.Resolve(remote.Addresses...) {
+					workloadEntries = append(workloadEntries, &v1alpha3.WorkloadEntry{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      fmt.Sprintf("import-%s-%d", importedSvc.Name, idx),
+							Namespace: importedSvc.Namespace,
+							Labels:    map[string]string{"federation.openshift-service-mesh.io/peer": "todo"},
+						},
+						Spec: istionetv1alpha3.WorkloadEntry{
+							Address: ip,
+							Labels:  maps.MergeCopy(importedSvc.Labels, map[string]string{"security.istio.io/tlsMode": "istio"}),
+							Ports:   makePortsMap(importedSvc.Ports, remote.GetPort()),
+							Network: remote.Network,
+						},
+					})
+				}
 			}
 		}
 	}
+
 	return workloadEntries, nil
 }
 
-func (cf *ConfigFactory) serviceEntryForRemoteFederationController() *v1alpha3.ServiceEntry {
-	remote := cf.cfg.MeshPeers.Remotes[0]
+func (cf *ConfigFactory) serviceEntryForRemoteFederationController(remote config.Remote) *v1alpha3.ServiceEntry {
 	se := &v1alpha3.ServiceEntry{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      remote.ServiceName(),
