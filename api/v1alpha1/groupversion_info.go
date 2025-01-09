593202f8e50444f1e3936cd60bccd8fc8c496622
Eoin Fennessy <efenness@redhat.com>
2025-01-09 14:05:18 +0000

    change project domain to openshift-service-mesh.io

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -14,7 +14,7 @@
 
 // Package v1alpha1 contains API Schema definitions for the federation v1alpha1 API group.
 // +kubebuilder:object:generate=true
-// +groupName=federation.federation.istio-ecosystem.io
+// +groupName=federation.openshift-service-mesh.io
 package v1alpha1
 
 import (
@@ -24,7 +24,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects.
-	GroupVersion = schema.GroupVersion{Group: "federation.federation.istio-ecosystem.io", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "federation.openshift-service-mesh.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
